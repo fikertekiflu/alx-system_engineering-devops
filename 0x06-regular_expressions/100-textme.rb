694fdf70af9c5fdc32bf9bbf1ec7320660e5f2a7
fikertekiflu <casssiopfiker@gmail.com>
2024-05-08 01:38:47 -0700

    edited

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,2 @@
-#!/usr/bin/env ruby
-#SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
-#RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
-#FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
-#
-#msg = SENDER + "," + RECEIVER + "," + FLAGS
-#puts msg
+#!/usr/bin/ env ruby
+puts ARGV[0].scan(/(?<-from:|to:|flags:).+?(?=\])/.join(',')
