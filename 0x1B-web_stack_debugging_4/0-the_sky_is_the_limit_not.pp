db82ed85158e139cac58a807a1d0919a75ec311b
fikertekiflu <casssiopfiker@gmail.com>
2024-08-22 11:27:12 -0700

    commited

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,13 @@
 # Increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec { 'fix-ulimit-for-nginx':
-  command => 'sed -i "s/ULIMIT=15/ULIMIT=4096/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
-  onlyif  => 'grep -q "ULIMIT=15" /etc/default/nginx', # Ensures it's only run if necessary
-  notify  => Exec['nginx-restart'],  # Triggers the Nginx restart
-}
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',
-  path    => ['/usr/local/sbin', '/usr/local/bin', '/sbin', '/bin', '/usr/sbin', '/usr/bin'],
-  refreshonly => true,  # Only restart if notified by the fix-ulimit-for-nginx exec
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
